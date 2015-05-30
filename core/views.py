f917e41bc9335602a7e76c6096616859a66915b5
Ali Kaafarani <ali@kvikshaug.no>
2015-05-31 01:59:08 +0200

    Get the price from the database, not socket

--- core/views.py
+++ core/views.py
@@ -1,9 +1,17 @@
+from django.conf import settings
 from django.shortcuts import render
 
-from core import price
+from core.models import Price
 
 def index(request):
+    price = Price.objects.order_by('datetime')[:1][0]
+    buy = price.price_float * settings.BUY_RATE
+    sell = price.price_float * settings.SELL_RATE
+    price = {
+        'buy': buy,
+        'sell': sell,
+    }
     context = {
-        'price': price.get_current(),
+        'price': price,
     }
     return render(request, 'core/index.html', context)

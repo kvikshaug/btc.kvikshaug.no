640d5de356d58b80f4d6ebb1c503ce88e144ea90
Ali Kaafarani <ali@kvikshaug.no>
2015-05-31 01:58:42 +0200

    Add property for converting price float to integer

--- core/models.py
+++ core/models.py
@@ -3,3 +3,11 @@ from django.db import models
 class Price(models.Model):
     price = models.PositiveIntegerField()
     datetime = models.DateTimeField()
+
+    @property
+    def price_float(self):
+        return self.price / 1000.0
+
+    @price_float.setter
+    def price_float(self, price):
+        self.price = round(price * 1000)

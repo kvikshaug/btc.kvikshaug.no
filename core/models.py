8fdb10789f1f3b2a8abf5804cce9b6e520fad608
Ali Kaafarani <ali@kvikshaug.no>
2015-05-31 11:53:29 +0200

    Convert price fields to decimal

--- core/models.py
+++ core/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Price(models.Model):
-    usdbtc = models.PositiveIntegerField()
-    usdnok = models.PositiveIntegerField()
-    rate_buy = models.PositiveIntegerField()
-    rate_sell = models.PositiveIntegerField()
+    usdbtc = models.DecimalField(max_digits=14, decimal_places=8, null=True)
+    usdnok = models.DecimalField(max_digits=12, decimal_places=8, null=True)
+    rate_buy = models.DecimalField(max_digits=3, decimal_places=2, null=True)
+    rate_sell = models.DecimalField(max_digits=3, decimal_places=2, null=True)
     datetime = models.DateTimeField(auto_now_add=True)
 
     def nokbtc(self):

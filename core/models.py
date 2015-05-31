f3d61570cd7799e55ff08e6109f71a5f711df500
Ali Kaafarani <ali@kvikshaug.no>
2015-05-31 02:47:30 +0200

    Add price calculation logic

--- core/models.py
+++ core/models.py
@@ -7,6 +7,21 @@ class Price(models.Model):
     rate_sell = models.PositiveIntegerField()
     datetime = models.DateTimeField(auto_now_add=True)
 
+    def nokbtc(self):
+        return Price.to_float(self.usdbtc) * Price.to_float(self.usdnok)
+
+    def buy_price(self):
+        return self.nokbtc() * self.buy_rate()
+
+    def sell_price(self):
+        return self.nokbtc() * self.sell_rate()
+
+    def buy_rate(self):
+        return self.rate_buy / 100.0
+
+    def sell_rate(self):
+        return self.rate_sell / 100.0
+
     @staticmethod
     def to_float(value):
         return value / 1000.0
--- core/views.py
+++ core/views.py
@@ -1,17 +1,9 @@
-from django.conf import settings
 from django.shortcuts import render
 
 from core.models import Price
 
 def index(request):
-    price = Price.objects.order_by('datetime')[:1][0]
-    buy = price.price_float * settings.BUY_RATE
-    sell = price.price_float * settings.SELL_RATE
-    price = {
-        'buy': buy,
-        'sell': sell,
-    }
     context = {
-        'price': price,
+        'price': Price.objects.order_by('datetime')[:1][0],
     }
     return render(request, 'core/index.html', context)

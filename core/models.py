4182e052f5e901b1ed1cfd7479ef277d63ae63e7
Ali Kaafarani <ali@kvikshaug.no>
2015-05-31 15:23:52 +0200

    Define __str__, not __unicode__
    The __unicode__ method is deprecated in Python 3

--- core/models.py
+++ core/models.py
@@ -20,8 +20,8 @@ class Price(models.Model):
     sell_rate = models.DecimalField(max_digits=3, decimal_places=2, null=True)
     datetime = models.DateTimeField(auto_now_add=True)
 
-    def __unicode__(self):
-        return u'%s: kr %s @ %s' % (self.pk, self.nokbtc, self.datetime)
+    def __str__(self):
+        return '%s: kr %s @ %s' % (self.pk, self.nokbtc, self.datetime)
 
     @property
     def nokbtc(self):

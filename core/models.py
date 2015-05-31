026e7aba6b5ad61fcff005c3eaf226230358e6de
Ali Kaafarani <ali@kvikshaug.no>
2015-05-31 02:11:12 +0200

    Set auto_now_add on price datetime model

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Price(models.Model):
     price = models.PositiveIntegerField()
-    datetime = models.DateTimeField()
+    datetime = models.DateTimeField(auto_now_add=True)
 
     @property
     def price_float(self):

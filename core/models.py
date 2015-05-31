8f237650fea2e1ab3a0e9eaaabae20dea5e80768
Ali Kaafarani <ali@kvikshaug.no>
2015-05-31 12:12:45 +0200

    Update model methods for decimal handling

--- core/models.py
+++ core/models.py
@@ -7,25 +7,14 @@ class Price(models.Model):
     rate_sell = models.DecimalField(max_digits=3, decimal_places=2, null=True)
     datetime = models.DateTimeField(auto_now_add=True)
 
+    @property
     def nokbtc(self):
-        return Price.to_float(self.usdbtc) * Price.to_float(self.usdnok)
+        return self.usdbtc * self.usdnok
 
+    @property
     def buy_price(self):
-        return self.nokbtc() * self.buy_rate()
+        return self.nokbtc * self.rate_buy
 
+    @property
     def sell_price(self):
-        return self.nokbtc() * self.sell_rate()
-
-    def buy_rate(self):
-        return self.rate_buy / 100.0
-
-    def sell_rate(self):
-        return self.rate_sell / 100.0
-
-    @staticmethod
-    def to_float(value):
-        return value / 1000.0
-
-    @staticmethod
-    def to_int(value):
-        return round(value * 1000)
+        return self.nokbtc * self.rate_sell

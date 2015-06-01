c7baa0a33945c5bfa28064c8447c9176d072514b
Ali Kaafarani <ali@kvikshaug.no>
2015-06-01 22:19:23 +0200

    Calculate and cache history per hour

--- core/chart.py
+++ core/chart.py
@@ -1,35 +1,61 @@
 from datetime import timedelta
 
 from django.conf import settings
+from django.core.cache import cache
 from django.utils import timezone
 
 from core.models import Price
 
 def get_price_history():
     now = timezone.now()
+    one_hour_ago = now - timedelta(hours=1)
     one_day_ago = now - timedelta(days=1)
 
     # Let's always start the chart at *the start of* an hour. This'll make caching easier
     one_day_ago -= timedelta(minutes=one_day_ago.minute, seconds=one_day_ago.second)
-
-    # We might need to include price objects from *CHART_GRANULARITY* minutes before the point 24h ago
-    # That's a bit arbitrary; a trade price *could* be from even before the granularity + 24h ago and still be the
-    # relevant price we want to display, but that's not handled for now - if there's no price within the granularity
-    # period, it'll be rendered as a *missing* value.
-    date_point_start = one_day_ago - timedelta(minutes=settings.CHART_GRANULARITY)
-
-    prices = Price.objects.filter(datetime__gte=date_point_start).order_by('datetime')
 
     price_history = [
         ['Tid', 'Kjøp', 'Salg'],
     ]
 
     date_point = one_day_ago
+    previous_price = None
+    while date_point < one_hour_ago:
+        hour_set = cache.get('price.history.by_hour.%s' % date_point.strftime("%d.%m.%Y.%H:00"))
+        if hour_set is None:
+            hour_set = _calculate_hour(date_point, previous_price, now)
+            cache.set('price.history.by_hour.%s' % date_point.strftime("%d.%m.%Y.%H:00"), hour_set, 60 * 60 * 24)
+        hour_history, previous_price = hour_set
+        price_history.extend(hour_history)
+        date_point += timedelta(hours=1)
+
+    # We're at the last hour; calculate that without caching it
+    hour_set = _calculate_hour(date_point, previous_price, now)
+    hour_history, previous_price = hour_set
+    price_history.extend(hour_history)
+    return price_history
+
+def _calculate_hour(date_point, previous_price, now):
+    an_hour_from_datepoint = date_point + timedelta(hours=1)
+
+    # Note: If this is the *first* hour of the chart, using the CHART_GRANULARITY time range to find the previous
+    # price is a bit arbitrary. A trade price *could* be from even before the granularity + 24h ago and still be the
+    # relevant price we want to display, but that's not handled for now - if there's no price within the granularity
+    # period, it'll be rendered as a *missing* value.
+    previous_date_point = date_point - timedelta(minutes=settings.CHART_GRANULARITY)
+
+    prices = Price.objects.filter(
+        datetime__gte=previous_date_point,
+        datetime__lte=an_hour_from_datepoint,
+    ).order_by('datetime')
+
     price_index = 0
     price_count = len(prices)
-    previous_date_point = date_point - timedelta(minutes=settings.CHART_GRANULARITY)
-    relevant_price = None
-    while date_point <= now:
+    hour_history = []
+
+    # Iterate while we're within the our, and while we're not in the future (latter case applies for the final hour
+    # calculation)
+    while date_point <= an_hour_from_datepoint and date_point <= now:
         # Find the latest price within the granularity range. Keep an index in order to iterate the prices along with
         # the date ranges.
         while price_index < price_count:
@@ -40,7 +66,7 @@ def get_price_history():
                 continue
             elif price.datetime >= previous_date_point and price.datetime <= date_point:
                 # This price is within our range, set it as relevant
-                relevant_price = price
+                previous_price = price
 
                 # Iterate to see if next price might be relevant too
                 price_index += 1
@@ -52,16 +78,16 @@ def get_price_history():
             else:
                 raise Exception("This should never *ever* happen")
 
-        if relevant_price is None:
+        if previous_price is None:
             # This might be the case if it's an early range and there hasn't been any price recorded yet
             buy_price = None
             sell_price = None
         else:
             # The relevant price will now either be leftover from a previous range or a new relevant one
-            buy_price = float(round(relevant_price.buy_price, 2))
-            sell_price = float(round(relevant_price.sell_price, 2))
+            buy_price = float(round(previous_price.buy_price, 2))
+            sell_price = float(round(previous_price.sell_price, 2))
 
-        price_history.append([
+        hour_history.append([
             date_point.strftime("%H:%M"),
             buy_price,
             sell_price,
@@ -69,5 +95,4 @@ def get_price_history():
 
         previous_date_point = date_point
         date_point += timedelta(minutes=settings.CHART_GRANULARITY)
-
-    return price_history
+    return hour_history, previous_price

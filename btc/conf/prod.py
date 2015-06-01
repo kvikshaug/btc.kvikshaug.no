d86c1292d329e3196cdbe4838d0c92cf32584c05
Ali Kaafarani <ali@kvikshaug.no>
2015-06-02 01:27:42 +0200

    Set cache backend to pylibmc

--- btc/conf/prod.py
+++ btc/conf/prod.py
@@ -88,7 +88,7 @@ STATICFILES_DIRS = (
 
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
         'LOCATION': '127.0.0.1:11211',
     }
 }

c3c6796b5d4374f04d9a662f682b0dbb165dea1a
Ali Kaafarani <ali@kvikshaug.no>
2015-06-05 17:12:44 +0200

    Log the applied configuration

--- btc/settings.py
+++ btc/settings.py
@@ -1,5 +1,6 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import logging
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 _configuration = os.environ.get('DJANGO_CONFIGURATION', 'dev').lower()
@@ -15,3 +16,6 @@ try:
     from btc.conf.local import *
 except ImportError:
     pass
+
+logger = logging.getLogger('btc')
+logger.info("Loaded settings from %s configuration..." % _configuration)

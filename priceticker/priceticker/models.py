cf9b63ea199c46b958a30ae27b431733935971d1
Ali Kaafarani <ali@kvikshaug.no>
2016-06-19 13:48:14 +0200

    Don't allow null values

--- priceticker/priceticker/models.py
+++ priceticker/priceticker/models.py
@@ -7,8 +7,8 @@ class Price(Base):
     __tablename__ = 'prices'
 
     id = Column(Integer, primary_key=True)
-    btcusd = Column(Numeric(precision=14, scale=8), nullable=True)
-    usdnok = Column(Numeric(precision=12, scale=8), nullable=True)
+    btcusd = Column(Numeric(precision=14, scale=8))
+    usdnok = Column(Numeric(precision=12, scale=8))
     datetime = Column(DateTime)
 
     def __repr__(self):
